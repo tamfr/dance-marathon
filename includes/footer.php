548a4cc6e72aa9cf0a8eef9e1262b193af89139f
tamfr <ejm29@case.edu>
2013-12-12 20:44:25 -0500

    Change everything pretty much.  Big things, php driven inlcudes, bootstrap 3, and lots more.

---
+++ includes/footer.php
@@ -0,0 +1,6 @@
+<div id="footer">
+	<div class="container">
+		<p class="pull-right"><a href="#">Back to top</a></p>
+		<p>&copy; 2013 Dance Marathon at Case Western Reserve University &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+	</div>
+</div><!-- /.footer -->
---
+++ includes/navigation.php
@@ -0,0 +1,28 @@
+<div class="navbar-wrapper">
+	<div class="container">
+		<div class="navbar navbar-static-top">
+			<div class="container">
+				<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				  <span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="test.html">
+				<div id="logo"><img class="img-responsive" src="img/logo.png" /></div>
+				<div id="logo-text-wrap"><div id="logo-text">Dance Marathon<br /><span id="logo-subtext">at Case Western Reserve University</span></div></div></a>
+				</div><!-- /.navbar-header -->
+				<div class="navbar-collapse collapse">
+				  <ul class="nav navbar-nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li><a href="#about">Founders</a></li>
+					<li><a href="downloads/Constitution-of-Dance-Marathon-at-CWRU.pdf">Constitution</a></li>
+					<li><a href="http://www.spartanthon.org">SpartanThon</a></li>
+					<li><a href="#">Contact</a></li>
+				  </ul>
+				</div><!-- /.navbar-collapse -->
+			</div><!-- /.container -->
+		</div><!-- /.navbar -->
+	</div><!-- /.container -->
+</div><!-- /.navbar-wrapper -->
+
