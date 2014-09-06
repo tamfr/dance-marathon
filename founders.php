9d0db49061af82499ece5eafd69935fc2581ca16
tamfr <ejm29@case.edu>
2014-09-06 13:52:24 -0400

    Update founders page.

--- founders.php
+++ founders.php
@@ -12,7 +12,51 @@
 <div id="wrap">
 
 	<?php include_once("includes/navigation.php") ?>
+	
+	<div id="myCarousel" class="carousel slide">
+      <!-- Indicators -->
+      <ol class="carousel-indicators">
+      </ol>
+      <div class="carousel-inner">
+        <div class="item active">
+          <img src="img/slide-05.jpg" alt="">
+          <div class="container">
+            <div class="carousel-caption">
+              <h1>We that Prosper, Love, Laugh, and Learn.</h1>
+              <p>From humble backgrounds to humble beginnings, Dance Marathon was born out of love and family.</p>
+              <p><a class="btn btn-large btn-primary" href="#more">Meet Us</a></p>
+            </div>
+          </div>
+        </div>
 
+      </div>
+    </div><!-- /.carousel -->
+	
+<div class="container marketing constitution" id="more">
+
+	<div class="row">
+		<div class="col-xs-12 col-md-8">
+		<h2>Leadership</h2>
+		<ul>
+			<li>Eric Mott</li>
+			<li>Anna Czekaj</li>
+			<li>Mark Beatty</li>
+		</ul>
+		<h2>Key Members</h2>
+		<ul>
+			<li>Alex Allegretto</li>
+			<li>Caitlin Bowerman</li>
+			<li>Sarah Greenlee</li>
+			<li>Christine Ng</li>
+			<li>Paul Proctor</li>
+		</ul>
+		
+		</div><!-- /span -->
+	</div><!-- /.row -->
+	
+	<hr class="story-divider">
+</div><!-- /.container -->
+	
 </div><!-- /#wrap -->
 
 <!-- FOOTER -->
